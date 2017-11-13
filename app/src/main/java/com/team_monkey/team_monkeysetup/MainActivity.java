9a6c237a4c98168eef3cb6bf8753bfc5e8316b32
noahhamilton <noah_hamilton@taylor.edu>
2017-11-12 19:23:09 -0500

    this is erroring out but will fix

--- app/src/main/java/com/team_monkey/team_monkeysetup/MainActivity.java
+++ app/src/main/java/com/team_monkey/team_monkeysetup/MainActivity.java
@@ -16,6 +16,93 @@ public class MainActivity extends AppCompatActivity {
         buffer = new Buffer(this);
         buffer.ApplyTestClip("Hello world!");
         buffer.SetMaxSize(25);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            //Intent for settings page
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            //Start Activity
+            startActivity(settingsIntent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    RecentTab recentTab = new RecentTab();
+                    return recentTab;
+                case 1:
+                    ClipboardTab clipboardTab = new ClipboardTab();
+                    return clipboardTab;
+                case 2:
+                    FavoritesTab favoritesTab = new FavoritesTab();
+                    return favoritesTab;
+
+            }
+            return null;
+
+        }
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "RECENT";
+                case 1:
+                    return "CLIPBOARD";
+                case 2:
+                    return "FAVORITES";
+            }
+            return null;
+        }
     }
 
     protected void onResume(){
