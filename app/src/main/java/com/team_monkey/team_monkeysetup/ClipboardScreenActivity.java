a4feb0c646c975c26c40c4795aa4c4f43619f78a
noahhamilton <noah_hamilton@taylor.edu>
2017-11-15 17:30:18 -0500

    Clipboard interface and settings page

---
+++ app/src/main/java/com/team_monkey/team_monkeysetup/ClipboardScreenActivity.java
@@ -0,0 +1,185 @@
+package com.team_monkey.team_monkeysetup;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.design.widget.TabLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ClipboardScreenActivity extends AppCompatActivity {
+    Buffer buffer;
+
+    SharedPreferences preferences;
+    Gson gson;
+    String PREF_NAME = "BuffClip";
+    String BUFFER = "Buffer";
+
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     */
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.clipboard_screen);
+        preferences = getSharedPreferences(PREF_NAME, 0);
+        gson = new Gson();
+        loadBuffer();
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+
+        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
+    }
+
+    protected void onResume() {
+        super.onResume();
+    }
+
+    protected void onPause() {
+        super.onPause();
+    }
+
+    protected void onStop() {
+        super.onStop();
+        saveBuffer();
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        buffer.RemoveClipboardEventListener();
+        saveBuffer();
+    }
+
+    private void loadBuffer() {
+        String bufferString = preferences.getString(BUFFER, "[]");
+        android.util.Log.d("bufferstring", bufferString);
+        if (bufferString != "[]") {
+            LinkedList<String> bufferList = new LinkedList<>((ArrayList<String>) gson.fromJson(bufferString, new TypeToken<List<String>>() {
+            }.getType()));
+            buffer = new Buffer(this, bufferList);
+        } else {
+            buffer = new Buffer(this);
+        }
+    }
+
+    private void saveBuffer() {
+        String BufferString;
+        BufferString = gson.toJson(buffer.Data());
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString(BUFFER, BufferString);
+        editor.commit();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_clipboard_screen, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            //Intent for settings page
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            //Start Activity
+            startActivity(settingsIntent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    ClipboardTab clipboardTab = new ClipboardTab();
+                    return clipboardTab;
+                case 1:
+                    FavoritesTab favoritesTab = new FavoritesTab();
+                    return favoritesTab;
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            // Show 2 total pages.
+            return 2;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "CLIPBOARD";
+                case 1:
+                    return "FAVORITES";
+            }
+            return null;
+        }
+
+    }
+}
