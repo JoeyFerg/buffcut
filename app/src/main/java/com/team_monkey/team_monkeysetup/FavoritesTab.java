5732cae52384badac20c33daee328fc5e4ae1e76
JoeyFerg <joey.ferguson2@gmail.com>
2017-11-15 23:41:26 -0500

    Fixed formatting and setup for adding favorites and recent

--- app/src/main/java/com/team_monkey/team_monkeysetup/FavoritesTab.java
+++ app/src/main/java/com/team_monkey/team_monkeysetup/FavoritesTab.java
@@ -2,19 +2,11 @@ package com.team_monkey.team_monkeysetup;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-/**
- * Created by noahs on 11/12/2017.
- */
-
+import android.view.*;
 
 public class FavoritesTab extends Fragment {
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.favorites_tab, container, false);
         return rootView;
     }
