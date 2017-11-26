f7f5b25a1bfcb799cc425285552fa3f956894187
jbernard <jbernard3396@gmail.com>
2017-11-26 13:35:53 -0500

    Add dialog

--- app/src/main/java/com/team_monkey/team_monkeysetup/OpenOverlay.java
+++ app/src/main/java/com/team_monkey/team_monkeysetup/OpenOverlay.java
@@ -5,23 +5,14 @@ package com.team_monkey.team_monkeysetup;
  */
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 
 
 public class OpenOverlay extends Activity {
 
      protected void onCreate(Bundle savedInstanceState){
          super.onCreate(savedInstanceState);
-         Intent intent = getIntent();
-         Bundle extras = intent.getExtras();
-         assert extras != null;
-         Integer tmp = extras.getInt("testVal");
-         if (tmp == 0){
-             tmp = extras.getInt("testVal2");
-         }
-         Log.w("tag", ""+tmp);
-         setContentView(R.layout.testfile);
+         dialogue dialogueTest = new dialogue();
+         dialogueTest.show(getFragmentManager(), "tag1");
     }
 }
