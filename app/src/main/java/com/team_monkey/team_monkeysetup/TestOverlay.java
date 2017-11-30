206e701a65a3a7604e2e0a4ff829269531ec6c33
jbernard <jbernard3396@gmail.com>
2017-11-30 13:50:02 -0500

    make an hudiew situation

--- app/src/main/java/com/team_monkey/team_monkeysetup/TestOverlay.java
+++ app/src/main/java/com/team_monkey/team_monkeysetup/TestOverlay.java
@@ -1,50 +1,49 @@
 package com.team_monkey.team_monkeysetup;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.graphics.PixelFormat;
 import android.os.IBinder;
 import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.team_monkey.team_monkeysetup.R;
 
-public class TestOverlay extends Service {
+class HUDView extends ViewGroup {
+    private Paint mLoadPaint;
 
-    private WindowManager windowManager;
-    private ImageView chatHead;
+    public HUDView(Context context) {
+        super(context);
+        Toast.makeText(getContext(),"HUDView", Toast.LENGTH_LONG).show();
 
-    @Override public IBinder onBind(Intent intent) {
-        // Not used
-        return null;
-    }
-
-    @Override public void onCreate() {
-        super.onCreate();
-
-        windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
-
-        chatHead = new ImageView(this);
-        chatHead.setImageResource(R.drawable.ic_launcher);
-
-        WindowManager.LayoutParams params = new WindowManager.LayoutParams(
-                WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.TYPE_PHONE,
-                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
-                PixelFormat.TRANSLUCENT);
-
-        params.gravity = Gravity.TOP | Gravity.LEFT;
-        params.x = 0;
-        params.y = 100;
-
-        windowManager.addView(chatHead, params);
+        mLoadPaint = new Paint();
+        mLoadPaint.setAntiAlias(true);
+        mLoadPaint.setTextSize(10);
+        mLoadPaint.setARGB(255, 255, 0, 0);
     }
 
     @Override
-    public void onDestroy() {
-        super.onDestroy();
-        if (chatHead != null) windowManager.removeView(chatHead);
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawText("Hello World", 5, 15, mLoadPaint);
+    }
+
+    @Override
+    protected void onLayout(boolean arg0, int arg1, int arg2, int arg3, int arg4) {
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        //return super.onTouchEvent(event);
+        Toast.makeText(getContext(),"onTouchEvent", Toast.LENGTH_LONG).show();
+        return true;
     }
 }
