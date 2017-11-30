cc1661a3ac25ccd9c6e6fae1c9a1e5eb1597c642
jbernard <jbernard3396@gmail.com>
2017-11-30 12:00:22 -0500

    Create a window majiger

--- app/src/main/java/com/team_monkey/team_monkeysetup/OpenOverlay.java
+++ app/src/main/java/com/team_monkey/team_monkeysetup/OpenOverlay.java
@@ -4,15 +4,82 @@ package com.team_monkey.team_monkeysetup;
  * Created by jbern on 11/13/2017.
  */
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.view.Gravity;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import static android.support.v4.app.ActivityCompat.startActivityForResult;
 
 
 public class OpenOverlay extends Activity {
+    public final int REQUEST_CODE = 60696;
+    private WindowManager windowManager;
+    private ImageView chatHead;
 
-     protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState){
          super.onCreate(savedInstanceState);
-         dialogue dialogueTest = new dialogue();
-         dialogueTest.show(getFragmentManager(), "tag1");
+         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SYSTEM_ALERT_WINDOW},0);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!Settings.canDrawOverlays(this)) {
+                Intent intent = new Intent(this, TestOverlay.class);
+                startService(intent);
+            } else {
+                requestPermissions();
+            }
+        }
+
+
+    }
+
+    private void requestPermissions() {
+        if (android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.M) {
+            return;
+        }
+
+        Intent myIntent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION);
+        myIntent.setData(Uri.parse("package:" + getPackageName()));
+        startActivityForResult(myIntent, REQUEST_CODE);
+    }
+
+
+
+    @Override public void onActivityResult(int requestCode, int resultCode, Intent data){
+        windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+
+        chatHead = new ImageView(this);
+        chatHead.setImageResource(R.drawable.ic_launcher);
+
+        WindowManager.LayoutParams params = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                        params = new WindowManager.LayoutParams(
+                                WindowManager.LayoutParams.WRAP_CONTENT,
+                                WindowManager.LayoutParams.WRAP_CONTENT,
+                                WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
+                                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                                PixelFormat.TRANSLUCENT);
+                    } else {
+                        params = new WindowManager.LayoutParams(
+                                WindowManager.LayoutParams.WRAP_CONTENT,
+                                WindowManager.LayoutParams.WRAP_CONTENT,
+                                WindowManager.LayoutParams.TYPE_PHONE,
+                                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                                PixelFormat.TRANSLUCENT);
+                    }
+        }
+        params.gravity = Gravity.TOP | Gravity.LEFT;
+        params.x = 0;
+        params.y = 100;
+
+        windowManager.addView(chatHead, params);
     }
 }
